c38f7309119f8f85a4575e4d83c9c9544b9d7abe
karimkoe <koehler.karim@gmail.com>
2020-11-22 22:08:01 +0100

    Added index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Wochenkarte</title>
+</head>
+<body>
+
+
+
+<div class="container">
+    <h1 class="mt-5 mb-3">Wochenkarte</h1>
+
+    <h3>Willkommen</h3>
+    <p>Diese Website verwendet Coockies.</p>
+    <button class="btn btn-warning">Akzeptieren</button>
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>
+<footer class="modal-footer">(c) <a href="https://www.instagram.com/davidkueng"> David Küng </a> & <a href="https://instagram.com/karimkoehler">Karim Köhler</a></footer>
+</html>
